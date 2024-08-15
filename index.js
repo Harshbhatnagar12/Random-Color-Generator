7c913c56667b5263bb5a60f01f6f63f9373204e0
Harsh Bhatnagar <harshbhatnagar2022@gmail.com>
2024-08-16 02:38:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Script.js initinializing")
+
+// let boxes = document.getElementsByClassName(".box")
+
+let boxes = document.querySelector(".container").children
+
+// Math.ceil are convert in GIF integer And other hand Math.random()*255 are genrate random number upto 255 
+
+function getRandomColor(){
+    let val1 = Math.ceil(0 + Math.random()*255);
+    let val2 = Math.ceil(0 + Math.random()*255);
+    let val3 = Math.ceil(0 + Math.random()*255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+}
+
+Array.from(boxes).forEach(e=>{
+    e.style.backgroundColor = getRandomColor()
+    // e.style.color = getRandomColor()
+    
+})
